8e52ac6f34f364a1626945ab9f0969df385c3d6f
Luis Garcia <lalejandro.1988@gmail.com>
2021-08-22 11:58:14 -0600

    Disenio PokemonApp

---
+++ src/pages/PokemonPage.vue
@@ -0,0 +1,21 @@
+<template>
+    <h1>¿Quién es este pokemón?</h1>
+    <!-- Img pokemon -->
+    <PokemonImage />
+    <PokemonOptions />
+    <!-- Opciones -->
+</template>
+
+<script>
+import PokemonImage from '@/components/PokemonImage.vue'
+import PokemonOptions from '@/components/PokemonOptions.vue'
+
+export default {
+    name: 'PokemonPage',
+    components: {
+        PokemonImage,
+        PokemonOptions,
+    }
+
+}
+</script>
